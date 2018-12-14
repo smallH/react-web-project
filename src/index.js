671c127186067d64f110d7a3834ff4f2fd8866d3
smallH <472908522@qq.com>
2018-12-14 08:01:53 +0800

    更新

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import App from '@/views/App';
+import store from '@/store'
+import '@/assets/styles/base.css'
+import '@/assets/fonts/iconfont.css'
+import {SetAxiosConfig} from '@/assets/js/middleware'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+SetAxiosConfig(store);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+render(
+	<Provider store={store}>
+    	<App />
+  	</Provider>, document.getElementById('root')
+)

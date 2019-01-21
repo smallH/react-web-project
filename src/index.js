443c9b118c47d26f937fbfca8ba66376b352826f
smallH <472908522@qq.com>
2019-01-21 16:48:23 +0800

    更新

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux'
 import App from '@/containers/App';
 import store from '@/store'
 import '@/assets/styles/base.css'
-import '@/assets/fonts/iconfont.css'
+import '@/assets/fonts/iconfont.css' // 使用方法: <i class="iconfont icon-arrow_left" />
 import { SetAxiosConfig } from '@/assets/js/middleware'
 
 // 配置Axios
